895e035fb817f0e60ea34c16f075efe87aa442f7
yuzhongc <765944853@qq.com>
2021-10-26 01:13:49 +0800

    "python"

---
+++ demo_1024/demo_h.py
@@ -0,0 +1,20 @@
+"""
+python常见的第三方库（pypi网址：算法，AI，神经网络）
+pytest    类似unittest
+request
+
+"""
+
+"""
+pytest框架
+"""
+
+
+
+
+"""
+requests库，网络访问
+"""
+import requests
+r = requests.get('http://www.baidu.com')
+print(r.text)
